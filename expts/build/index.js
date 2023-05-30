1f1eaccd074cfa36317a6638edfbda9d9f458062
Laura Lima <lauralimaufam@gmail.com>
2023-05-30 16:11:52 -0300

    exercicio 5 - v1

--- expts/build/index.js
+++ expts/build/index.js
@@ -6,12 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const validEnv_1 = __importDefault(require("./utils/validEnv"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const morganBody = require("morgan-body");
+var morgan = require('morgan');
 dotenv_1.default.config();
 (0, validEnv_1.default)();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3333;
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+const LOGS_FOLDER = process.env.LOGS_FOLDER || "../logs";
+const log = fs_1.default.createWriteStream(path_1.default.join(__dirname, LOGS_FOLDER, "express.log"), { flags: "a" });
+app.use(morgan('combined', { stream: log }));
+app.use((req, res, next) => {
+    console.log(`Requisição ${req.method} ${req.url}`);
+    next();
+});
+app.get('/', (req, res) => {
+    res.send('Hello world!');
 });
 app.listen(PORT, () => {
     console.log(`Express app iniciada na porta ${PORT}.`);
--- expts/src/index.ts
+++ expts/src/index.ts
@@ -1,16 +1,33 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
 import validateEnv from './utils/validEnv';
+import fs from 'fs'
+import path from 'path'
+const morganBody = require("morgan-body")
+var morgan = require('morgan')
 
 dotenv.config();
 validateEnv();
 
 const app = express();
 const PORT = process.env.PORT || 3333;
+const LOGS_FOLDER = process.env.LOGS_FOLDER || "../logs";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world!");
+const log = fs.createWriteStream(
+    path.join(__dirname, LOGS_FOLDER, "express.log"), { flags: "a"}
+)
+
+app.use(morgan('combined', { stream: log }))
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(`Requisição ${req.method} ${req.url}`);
+    next();
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Express app iniciada na porta ${PORT}.`);
+    console.log(`Express app iniciada na porta ${PORT}.`);
 });
